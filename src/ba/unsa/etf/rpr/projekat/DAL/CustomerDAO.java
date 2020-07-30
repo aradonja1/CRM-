cf00b8330c4ec978dbc4edf62af2fe99a363cb1b
Adnan Radonja <aradonja1@etf.unsa.ba>
2020-07-30 23:17:10 +0200

    promijeni, dodaj klijenta, ugovore..

--- src/ba/unsa/etf/rpr/projekat/DAL/CustomerDAO.java
+++ src/ba/unsa/etf/rpr/projekat/DAL/CustomerDAO.java
@@ -17,7 +17,11 @@ public class CustomerDAO {
     private DatabaseConnection db = DatabaseConnection.getInstance();
     private Connection conn;
 
-    private PreparedStatement allCustomersStatement, getConnectionFromId, getServiceFromId, getPackageFromId, getAllContractsFromCustomer;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private PreparedStatement allCustomersStatement, getConnectionFromIdStatement, getServiceFromIdStatement, getPackageFromIdStatement,
+            getAllContractsForCustomerStatement, addCustomerStatement, getConnectionFromServiceAndPackageIdStatement, getIdForNewCustomerStatement,
+            addContractStatement, getIdForNewContractStatement, editCustomerStatement, getCurrentContractStatement, editCurrentContract;
 
     public CustomerDAO() {
         conn = db.getConn();
@@ -28,10 +32,20 @@ public class CustomerDAO {
         }
         try {
             allCustomersStatement = conn.prepareStatement("SELECT * FROM customer");
-            getConnectionFromId = conn.prepareStatement("SELECT * FROM connection WHERE id=?");
-            getServiceFromId = conn.prepareStatement("SELECT * FROM service WHERE id=?");
-            getPackageFromId = conn.prepareStatement("SELECT * FROM package WHERE id=?");
-            getAllContractsFromCustomer = conn.prepareStatement("SELECT * FROM contract WHERE customer_id=?");
+            getConnectionFromIdStatement = conn.prepareStatement("SELECT * FROM connection WHERE id=?");
+            getServiceFromIdStatement = conn.prepareStatement("SELECT * FROM service WHERE id=?");
+            getPackageFromIdStatement = conn.prepareStatement("SELECT * FROM package WHERE id=?");
+            getAllContractsForCustomerStatement = conn.prepareStatement("SELECT * FROM contract WHERE customer_id=?");
+
+            addCustomerStatement = conn.prepareStatement("INSERT INTO customer VALUES(?,?,?,?,?,?,?,?,?)");
+            getConnectionFromServiceAndPackageIdStatement = conn.prepareStatement("SELECT id FROM connection WHERE service_id=? AND package_id=?");
+            getIdForNewCustomerStatement = conn.prepareStatement("SELECT MAX(id)+1 FROM customer");
+            addContractStatement = conn.prepareStatement("INSERT INTO contract VALUES(?,?,?,?,?)");
+            getIdForNewContractStatement = conn.prepareStatement("SELECT MAX(id)+1 FROM contract");
+
+            editCustomerStatement = conn.prepareStatement("UPDATE customer SET first_name=?, last_name=?, email=?, adress=?, contact=?, begin_contract=?, end_contract=?, connection=? WHERE id=? ");
+            getCurrentContractStatement = conn.prepareStatement("SELECT *.a FROM contract a, customer b WHERE b.id=a.customer_id AND b.id=?");
+            editCurrentContract = conn.prepareStatement("UPDATE contract SET customer_id=?, begin_contract=?, end_contract=?, connection=? WHERE id=?");
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -53,18 +67,27 @@ public class CustomerDAO {
     }
 
     private Customer getCustomerFromResultSet(ResultSet rs) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-         try {
-             getConnectionFromId.setInt(1,  rs.getInt(9));
-             ResultSet rs2 = getConnectionFromId.executeQuery();
-             getServiceFromId.setInt(1, rs2.getInt(2));
-             getPackageFromId.setInt(1,  rs2.getInt(3));
-             ResultSet rs3 = getServiceFromId.executeQuery();
-             ResultSet rs4 = getPackageFromId.executeQuery();
-             return new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
-                    rs.getString(5), rs.getString(6), LocalDate.parse(rs.getString(7), formatter),
-                    LocalDate.parse(rs.getString(8), formatter), getServiceFromId(rs3.getInt(1), getPackageFromId(rs4.getInt(1))),
-                     null);
+          try {
+             getConnectionFromIdStatement.setInt(1,  rs.getInt(9));
+             ResultSet rs2 = getConnectionFromIdStatement.executeQuery();
+             Customer c = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                     rs.getString(5), rs.getString(6), LocalDate.parse(rs.getString(7), formatter),
+                     LocalDate.parse(rs.getString(8), formatter), getServiceFromConnectionResultSet(rs2), null);
+            c.setContracts(getContractsFromCustomer(c));
+            return c;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Service getServiceFromConnectionResultSet(ResultSet rs) {
+        try {
+            getServiceFromIdStatement.setInt(1, rs.getInt(2));
+            getPackageFromIdStatement.setInt(1,  rs.getInt(3));
+            ResultSet rs3 = getServiceFromIdStatement.executeQuery();
+            ResultSet rs4 = getPackageFromIdStatement.executeQuery();
+            return getServiceFromId(rs3.getInt(1), getPackageFromId(rs4.getInt(1)));
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -73,8 +96,8 @@ public class CustomerDAO {
 
     private Service getServiceFromId(int serviceId, Package aPackage) {
         try {
-            getServiceFromId.setInt(1, serviceId);
-            ResultSet rs = getServiceFromId.executeQuery();
+            getServiceFromIdStatement.setInt(1, serviceId);
+            ResultSet rs = getServiceFromIdStatement.executeQuery();
             return new Service(rs.getInt(1), rs.getString(2), aPackage);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -85,8 +108,8 @@ public class CustomerDAO {
     //vraća paket iz baze koji ima id = packageId
     private Package getPackageFromId(int packageId) {
         try {
-            getPackageFromId.setInt(1, packageId);
-            ResultSet rs = getPackageFromId.executeQuery();
+            getPackageFromIdStatement.setInt(1, packageId);
+            ResultSet rs = getPackageFromIdStatement.executeQuery();
             return new Package(rs.getInt(1), rs.getString(2));
         } catch (SQLException e) {
             e.printStackTrace();
@@ -96,21 +119,149 @@ public class CustomerDAO {
 
 
 
-    private Contract getContractsFromCustomer(Customer customer) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private ArrayList<Contract> getContractsFromCustomer(Customer customer) {
+        ArrayList<Contract> result = new ArrayList<>();
         try {
-            getAllContractsFromCustomer.setInt(1, customer.getId());
-            ResultSet rs = getAllContractsFromCustomer.executeQuery();
+            getAllContractsForCustomerStatement.setInt(1, customer.getId());
+            ResultSet rs = getAllContractsForCustomerStatement.executeQuery();
             while (rs.next()) {
-
+                getConnectionFromIdStatement.setInt(1,  rs.getInt(5));
+                ResultSet rs2 = getConnectionFromIdStatement.executeQuery();
+                result.add(new Contract(rs.getInt(1), LocalDate.parse(rs.getString(3), formatter), LocalDate.parse(rs.getString(4), formatter), getServiceFromConnectionResultSet(rs2)));
+                Contract c = new Contract(rs.getInt(1), LocalDate.parse(rs.getString(3), formatter), LocalDate.parse(rs.getString(4), formatter), getServiceFromConnectionResultSet(rs2));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public DatabaseConnection getDb() {
+        return db;
+    }
+
+    public ArrayList<Contract> getArchivedContracts(Customer customer) {
+        ArrayList<Contract> all = getContractsFromCustomer(customer);
+        ArrayList<Contract> all2 = customer.getContracts();
+        ArrayList<Contract> result = new ArrayList<>();
+        for (Contract c : all) {
+            if (c.getEndContract().isBefore(LocalDate.now())) {
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+    //kada se dodaje novi ugovor dodaje se i u tabelu contract ne samo customer
+
+    public void addCustomer(Customer customer) {
+        try {
+            //find id for new customer
+            ResultSet rs = getIdForNewCustomerStatement.executeQuery();
+            int id = rs.getInt(1);
+            customer.setId(id);
+            addCustomerStatement.setInt(1, customer.getId());
+            addCustomerStatement.setString(2, customer.getFirstName());
+            addCustomerStatement.setString(3, customer.getLastName());
+            addCustomerStatement.setString(4, customer.getEmail());
+            addCustomerStatement.setString(5, customer.getAdress());
+            addCustomerStatement.setString(6, customer.getContact());
+            addCustomerStatement.setString(7, customer.getBeginContract().format(formatter));
+            addCustomerStatement.setString(8, customer.getEndContract().format(formatter));
+            int connectionId = getConnectionId(customer);
+            addCustomerStatement.setInt(9, connectionId);
+            addCustomerStatement.execute();
+            //after we have added a customer, let's add a contract
+            addContract(customer, connectionId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addContract(Customer customer, int connectionId) {
+        ResultSet rs2 = null;
+        try {
+            rs2 = getIdForNewContractStatement.executeQuery();
+            int contractId = rs2.getInt(1);
+            addContractStatement.setInt(1, contractId);
+            addContractStatement.setInt(2, customer.getId());
+            addContractStatement.setString(3, customer.getBeginContract().format(formatter));
+            addContractStatement.setString(4, customer.getEndContract().format(formatter));
+            addContractStatement.setInt(5, connectionId);
+            addContractStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editCustomer(Customer customer) {
+        try {
+            editCustomerStatement.setString(1, customer.getFirstName());
+            editCustomerStatement.setString(2, customer.getLastName());
+            editCustomerStatement.setString(3, customer.getEmail());
+            editCustomerStatement.setString(4, customer.getAdress());
+            editCustomerStatement.setString(5, customer.getContact());
+            editCustomerStatement.setString(6, customer.getBeginContract().format(formatter));
+            editCustomerStatement.setString(7, customer.getEndContract().format(formatter));
+            int connectionId = getConnectionId(customer);
+            editCustomerStatement.setInt(8, connectionId);
+            editCustomerStatement.setInt(9, customer.getId());
+            editCustomerStatement.execute();
+            //razmisli sta uraditi sa ugovorom kad izmjenjujes korisnika...
+            //zakljuci trenutni i kreiraj novi
+            getCurrentContractStatement.setInt(1, customer.getId());
+            ResultSet rs = getCurrentContractStatement.executeQuery();
+            Contract c = getContractFromResultSet(rs);
+            concludeCurrentContract(c);
+            addContract(customer, connectionId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+     }
+
+    private void concludeCurrentContract(Contract contract) {
+        try {
+            editCurrentContract.setString(3, LocalDate.now().format(formatter));
+            editCurrentContract.setInt(5, contract.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editCurrentContract(Contract contract) {
+        //...
+    }
+
+    private Contract getContractFromResultSet(ResultSet rs) {
+        try {
+            int id = rs.getInt(1);
+            int customerId = rs.getInt(2);
+            LocalDate beginContract = LocalDate.parse(rs.getString(3), formatter);
+            LocalDate endContract = LocalDate.parse(rs.getString(4), formatter);
+            int connectionId = rs.getInt(5);
+            getConnectionFromIdStatement.setInt(1, connectionId);
+            ResultSet rs2 = getConnectionFromIdStatement.executeQuery();
+            Service s = getServiceFromConnectionResultSet(rs2);
+            return new Contract(id, beginContract, endContract, s);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public DatabaseConnection getDb() {
-        return db;
-    }
+    private int getConnectionId(Customer customer) {
+         int packageId = customer.getService().getaPackage().getId();
+         int serviceId = customer.getService().getId();
+         try {
+             getConnectionFromServiceAndPackageIdStatement.setInt(1, serviceId);
+             getConnectionFromServiceAndPackageIdStatement.setInt(2, packageId);
+             ResultSet rs = getConnectionFromServiceAndPackageIdStatement.executeQuery();
+             return rs.getInt(1);
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+         return 0;
+     }
+
+
 }
--- test/ba/unsa/etf/rpr/projekat/DAL/CustomerDAOTest.java
+++ test/ba/unsa/etf/rpr/projekat/DAL/CustomerDAOTest.java
@@ -1,11 +1,16 @@
 package ba.unsa.etf.rpr.projekat.DAL;
 
+import ba.unsa.etf.rpr.projekat.Contract;
 import ba.unsa.etf.rpr.projekat.Customer;
+import ba.unsa.etf.rpr.projekat.Package;
+import ba.unsa.etf.rpr.projekat.Service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +31,36 @@ class CustomerDAOTest {
         assertEquals("Rijad", customers.get(0).getFirstName());
         assertEquals("Paketi prometa interneta", customers.get(0).getService().getName());
         assertEquals("Neo", customers.get(0).getService().getaPackage().getName());
+    }
+
+    @Test
+    void getArchivedContracts() {
+        CustomerDAO c = new CustomerDAO();
+        ArrayList<Customer> customers = c.customers();
+        ArrayList<Contract> result = c.getArchivedContracts(customers.get(0));
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void addCustomer() {
+        CustomerDAO c = new CustomerDAO();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Service s = new Service(1, "Paketi prometa razgovora", new Package(5, "Mobilna telefonija"));
+        Customer customer = new Customer(2, "Arman", "Radonja", "arman.radonja@gmail.com", "Sarajevo", "555555", LocalDate.now(), LocalDate.parse("27/02/2025", formatter), s, null);
+        c.addCustomer(customer);
+        ArrayList<Customer> customers = c.customers();
+        assertEquals(2, customers.size());
+    }
+
+    @Test
+    void editCustomer() {
+        CustomerDAO c = new CustomerDAO();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Service s = new Service(1, "Paketi prometa razgovora", new Package(5, "Mobilna telefonija"));
+        Customer customer = new Customer(1, "Arman", "Radonja", "arman.radonja@gmail.com", "Sarajevo", "555555", LocalDate.now(), LocalDate.parse("27/02/2025", formatter), s, null);
+        c.editCustomer(customer);
+        ArrayList<Customer> customers = c.customers();
+        assertEquals("Arman", customers.get(0).getFirstName());
     }
 
 /*
