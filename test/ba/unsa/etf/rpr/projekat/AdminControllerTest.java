eec83335300e759b14593970bd383fd94b85d715
Adnan Radonja <aradonja1@etf.unsa.ba>
2020-08-16 22:00:49 +0200

    dodani testovi u  AdminControllerTest.java

--- test/ba/unsa/etf/rpr/projekat/AdminControllerTest.java
+++ test/ba/unsa/etf/rpr/projekat/AdminControllerTest.java
@@ -90,6 +90,37 @@ class AdminControllerTest {
     }
 
     @Test
+    public void onActionbtnAdd(FxRobot robot) {
+        robot.clickOn("#btnAdd");
+
+        ArrayList<Employee> listEmployees = employeeDAO.employees();
+        assertEquals(2, listEmployees.size());
+        robot.clickOn("#fldName");
+        robot.write("Novi");
+        robot.clickOn("#fldSurname");
+        robot.write("Uposlenik");
+        robot.clickOn("#fldUsername");
+        robot.write("noviu1");
+        robot.clickOn("#fldPassword");
+        robot.write("novi123");
+        robot.clickOn("#fldRptPassword");
+        robot.write("novi123");
+        robot.clickOn("#btnOk");
+
+        listEmployees = employeeDAO.employees();
+        assertEquals(3, listEmployees.size());
+
+        boolean ok = false;
+        for (Employee e : listEmployees) {
+            if (e.getFirstName().equals("Novi") && e.getLastName().equals("Uposlenik"))
+                ok = true;
+        }
+        assertTrue(ok);
+
+    }
+
+
+    @Test
     public void onActionEdit (FxRobot robot) {
         Button btdEdit = robot.lookup("#btnEdit").queryAs(Button.class);
         assertNotNull(btdEdit);
@@ -130,6 +161,86 @@ class AdminControllerTest {
     }
 
     @Test
+    public void onActionEditEmployee2(FxRobot robot) {
+        robot.clickOn("Senid Hodžić");
+
+        robot.clickOn("#edit");
+        robot.clickOn("#edit2");
+
+        ArrayList<Employee> listEmployees = employeeDAO.employees();
+        assertEquals(2, listEmployees.size());
+
+        robot.clickOn("#fldName");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+
+        robot.write("Izmjena");
+        robot.clickOn("#fldSurname");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("Uposlenika");
+        robot.clickOn("#fldUsername");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena1");
+        robot.clickOn("#fldPassword");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena123");
+        robot.clickOn("#fldRptPassword");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena123");
+        robot.clickOn("#btnOk");
+
+        listEmployees = employeeDAO.employees();
+        assertEquals(2, listEmployees.size());
+
+        boolean ok = false;
+        for (Employee e : listEmployees) {
+            if (e.getFirstName().equals("Izmjena") && e.getLastName().equals("Uposlenika"))
+                ok = true;
+        }
+        assertTrue(ok);
+
+    }
+
+    @Test
+    public void onActionEditEmployee(FxRobot robot) {
+        robot.clickOn("Senid Hodžić");
+
+        robot.clickOn("#editEmployeeBtn");
+        onActionEdit(robot);
+
+        ArrayList<Employee> listEmployees = employeeDAO.employees();
+        assertEquals(2, listEmployees.size());
+
+        robot.clickOn("#fldName");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+
+        robot.write("Izmjena");
+        robot.clickOn("#fldSurname");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("Uposlenika");
+        robot.clickOn("#fldUsername");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena1");
+        robot.clickOn("#fldPassword");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena123");
+        robot.clickOn("#fldRptPassword");
+        robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+        robot.write("izmjena123");
+        robot.clickOn("#btnOk");
+
+        listEmployees = employeeDAO.employees();
+        assertEquals(2, listEmployees.size());
+
+        boolean ok = false;
+        for (Employee e : listEmployees) {
+            if (e.getFirstName().equals("Izmjena") && e.getLastName().equals("Uposlenika"))
+                ok = true;
+        }
+        assertTrue(ok);
+
+    }
+
+    @Test
     public void onActionDelete(FxRobot robot) {
         ArrayList<Employee> employees = employeeDAO.employees();
         assertEquals(2, employees.size());
@@ -148,11 +259,83 @@ class AdminControllerTest {
     }
 
     @Test
+    public void onActionDeleteEmployee(FxRobot robot) {
+        ArrayList<Employee> employees = employeeDAO.employees();
+        assertEquals(2, employees.size());
+
+        robot.clickOn("Senid Hodžić");
+
+        robot.clickOn("#btnDeleteEmployee");
+
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
+
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+
+        employees = employeeDAO.employees();
+        assertEquals(1, employees.size());
+
+    }
+
+    @Test
+    public void onActionDeleteEmployee2(FxRobot robot) {
+        ArrayList<Employee> employees = employeeDAO.employees();
+        assertEquals(2, employees.size());
+
+        robot.clickOn("Senid Hodžić");
+
+        robot.clickOn("#edit");
+        robot.clickOn("#dltCstmr");
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
+
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+
+        employees = employeeDAO.employees();
+        assertEquals(1, employees.size());
+
+    }
+
+    @Test
     public void onActionCustomers(FxRobot robot) {
         ArrayList<Customer> customers = customerDAO.nonarchivedCustomers();
         assertEquals(1, customers.size());
 
         robot.clickOn("#btnCustomers");
+
+        robot.lookup("#fldFilter").tryQuery().isPresent();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.hide());
+        TextField filter = robot.lookup("#fldFilter").queryAs(TextField.class);
+        assertNotNull(filter);
+
+        Stage stage = (Stage) filter.getScene().getWindow();
+        Platform.runLater(() -> stage.close());
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.show());
+    }
+
+    @Test
+    public void onActionViewCustomers(FxRobot robot) {
+        robot.clickOn("#view");
+        robot.clickOn("#cstms");
+        ArrayList<Customer> customers = customerDAO.nonarchivedCustomers();
+        assertEquals(1, customers.size());
+
 
         robot.lookup("#fldFilter").tryQuery().isPresent();
 
@@ -210,6 +393,38 @@ class AdminControllerTest {
     }
 
     @Test
+    public void onActionViewService(FxRobot robot) {
+        robot.clickOn("#view");
+        robot.clickOn("#srvs");
+        ArrayList<Service> services = customerDAO.services();
+        assertEquals(3, services.size());
+
+
+        robot.lookup("#fldName").tryQuery().isPresent();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.hide());
+
+        TextField name = robot.lookup("#fldName").queryAs(TextField.class);
+        assertNotNull(name);
+
+        robot.clickOn("#btnCancel");
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.show());
+    }
+
+    @Test
     public void onActionAddPackage(FxRobot robot) {
         ArrayList<Package> packages = customerDAO.packages();
         assertEquals(8, packages.size());
@@ -239,6 +454,84 @@ class AdminControllerTest {
         Platform.runLater(() -> theStage.show());
     }
 
+    @Test
+    public void onActionAbout(FxRobot robot) {
+        robot.clickOn("#help");
+        robot.clickOn("#abt");
 
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
 
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void onActionViewPackage(FxRobot robot) {
+        robot.clickOn("#view");
+        robot.clickOn("#pcks");
+        ArrayList<Package> packages = customerDAO.packages();
+        assertEquals(8, packages.size());
+
+        robot.lookup("#fldName").tryQuery().isPresent();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.hide());
+
+        TextField name = robot.lookup("#fldName").queryAs(TextField.class);
+        assertNotNull(name);
+        robot.clickOn("#btnCancel");
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Platform.runLater(() -> theStage.show());
+    }
+
+    @Test
+    public void onActionWriteXML(FxRobot robot) {
+        robot.clickOn("#wXML");
+
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
+
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+
+        assertTrue(true);
+
+        DialogPane dialogPane2 = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton2 = (Button) dialogPane2.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton2);
+
+        try {
+            Thread.sleep(3100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(true);
+    }
+
+    @Test
+    public void onActionReadXML(FxRobot robot) {
+        robot.clickOn("#rXML");
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
+
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+
+        assertTrue(true);
+
+    }
 }
